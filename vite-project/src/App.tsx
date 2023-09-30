1601a74597ade9ee3047b92957beb405f3945d93
jkea1 <jinkyng7890@gmail.com>
2023-10-01 00:10:37 +0900

    Revert "feat : App.tsx 수정"
    This reverts commit 0c454e89869365c23ee20837437bc7e173ed1de8.

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,17 +1,16 @@
-import Button from '@/components/Button';
-import { Box } from '@chakra-ui/react';
+import Button from '@/components/Button'
+import { Box } from '@chakra-ui/react'
 function App() {
+
   return (
     <>
       <div>프로젝트 초기 세팅 완료</div>
-      <Box bg="tomato" w="100%" p={4} color="white">
+      <Box bg='tomato' w='100%' p={4} color='white'>
         This is the Box
       </Box>
-      <Button />
-      <h1>아무거나 쓰기</h1>
-      <div>프로젝트 초기 세팅 완료!!!!!!!!우히히</div>
+      <Button/>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
