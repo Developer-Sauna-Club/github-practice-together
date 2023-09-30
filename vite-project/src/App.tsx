d7e32d821f016337ecb1df6d378c5ba81ebffb2e
jkea1 <jinkyng7890@gmail.com>
2023-09-30 23:22:57 +0900

    feat: box layout 적용

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,8 +1,12 @@
+import { Box } from '@chakra-ui/react'
 function App() {
 
   return (
     <>
       <div>프로젝트 초기 세팅 완료</div>
+      <Box bg='tomato' w='100%' p={4} color='white'>
+        This is the Box
+      </Box>
     </>
   )
 }
---
+++ vite-project/src/pages/temp.tsx
@@ -0,0 +1,5 @@
+import { Box } from '@chakra-ui/react'
+
+<Box bg='tomato' w='100%' p={4} color='white'>
+  This is the Box
+</Box>
