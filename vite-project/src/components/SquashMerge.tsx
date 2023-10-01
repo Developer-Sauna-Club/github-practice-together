294a0f38d950d3d0fa92f94b5ab82559a7016b48
Kimdaeun <104294861+GBAJS754@users.noreply.github.com>
2023-10-01 16:23:41 +0900

    Merge practice (#9)
    * 스쿼시연습
    
    * 머지 진행시켜
    
    * 이래도..?

---
+++ vite-project/src/components/SquashMerge.tsx
@@ -0,0 +1,12 @@
+import Button from './Button';
+
+const SquashMerge = () => {
+  return (
+    <div>
+      <Button></Button>
+      <h2>이것도 한번 추가해보고 머지 진행시켜~</h2>
+    </div>
+  );
+};
+
+export default SquashMerge;
