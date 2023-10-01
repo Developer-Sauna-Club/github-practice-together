ea98ed69dde72ef276e1631ca05869aa76fb3112
daeun <gbajs754@gmail.com>
2023-10-01 16:16:02 +0900

    이래도..?

--- vite-project/src/components/SquashMerge.tsx
+++ vite-project/src/components/SquashMerge.tsx
@@ -1,7 +1,9 @@
+import Button from './Button';
+
 const SquashMerge = () => {
   return (
     <div>
-      <h1>이거 글자 바꿔보고</h1>
+      <Button></Button>
       <h2>이것도 한번 추가해보고 머지 진행시켜~</h2>
     </div>
   );
